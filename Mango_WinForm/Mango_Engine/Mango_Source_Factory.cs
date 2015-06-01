de5e249de465eb2ef08b3458e35c313b27a440cc
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-06-02 00:22:43 +0700

    Build number now are in 0.5, change title for About page, add base code for Factory Class.

---
+++ Mango_WinForm/Mango_Engine/Mango_Source_Factory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango_Engine
+{
+    internal class Mango_Source_Factory
+    {
+        #region Fields
+        /*Fields*/
+        #endregion
+
+        #region Property
+        /*Property*/
+        #endregion
+
+        #region Constructor
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        public static Mango_Source get_new(string type)
+        {
+
+        }
+        #endregion
+    }
+}
--- Mango_WinForm/Mango_WinForm/Form2.Designer.cs
+++ Mango_WinForm/Mango_WinForm/Form2.Designer.cs
@@ -31,17 +31,19 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(13, 12);
+            this.textBox1.Location = new System.Drawing.Point(40, 7);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(377, 35);
+            this.textBox1.Size = new System.Drawing.Size(251, 23);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Created by Code-ScottLe";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -50,10 +52,11 @@
             // 
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(13, 53);
+            this.textBox2.Location = new System.Drawing.Point(40, 34);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Name = "textBox2";
             this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(377, 22);
+            this.textBox2.Size = new System.Drawing.Size(251, 22);
             this.textBox2.TabIndex = 1;
             this.textBox2.Text = "reddit.com/u/AMRAAM_Missiles/";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -61,24 +64,37 @@
             // textBox3
             // 
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Location = new System.Drawing.Point(84, 92);
+            this.textBox3.Location = new System.Drawing.Point(56, 60);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(233, 19);
+            this.textBox3.Size = new System.Drawing.Size(155, 13);
             this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox4.Location = new System.Drawing.Point(13, 122);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(302, 13);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Special Thanks to Norway Scan for picking up Domestic Kanojo";
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(402, 140);
+            this.ClientSize = new System.Drawing.Size(330, 166);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form2";
-            this.Text = "Form2";
+            this.Text = "About";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -89,5 +105,6 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
