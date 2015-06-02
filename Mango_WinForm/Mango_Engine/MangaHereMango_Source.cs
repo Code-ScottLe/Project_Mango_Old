43ad80528a58e7745276a4f2c96e2184020930e7
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-06-02 17:40:32 +0700

    Start building block for Manga Here.

---
+++ Mango_WinForm/Mango_Engine/MangaHereMango_Source.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango_Engine
+{
+    public class MangaHereMango_Source : Mango_Source
+    {
+        #region Fields
+        /*Fields*/
+        #endregion
+
+        #region Propeties
+        /*Properties*/
+        #endregion
+
+        #region Constructor
+        /*Constructors*/
+        #endregion
+
+        #region Methods
+        /*Methods*/
+        #endregion
+    }
+}
