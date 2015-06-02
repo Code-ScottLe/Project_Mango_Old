2facf93463999c7b6b41d29720c4b2844104fb37
Code-ScottLe <AMRAAMMissiles@hotmail.com>
2015-06-02 17:56:06 +0700

    Source Code is completed.

--- Mango_WinForm/Mango_Engine/MangaHereMango_Source.cs
+++ Mango_WinForm/Mango_Engine/MangaHereMango_Source.cs
@@ -18,10 +18,57 @@ namespace Mango_Engine
 
         #region Constructor
         /*Constructors*/
+
+        MangaHereMango_Source() : base()
+        {
+            //default constructor. Call base.
+            _source_name = "MangaHere";
+        }
+
+        public MangaHereMango_Source(string url_source) : base(url_source)
+        {
+            //Create a new instace of MangaHereMango_Source, an object represent mangahere.co source for mango.
+            _source_name = "MangaHere";
+        }
         #endregion
 
         #region Methods
         /*Methods*/
+        public override void init()
+        {
+ 	        base.init();
+        }
+
+        public override async Task initAsync()
+        {
+            
+        }
+
+        public override bool next_page()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override async Task<bool> next_page_Async()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string get_image_url()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<string> get_image_url_Async()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string get_url()
+        {
+            return current_url;
+        }
+
         #endregion
     }
 }
